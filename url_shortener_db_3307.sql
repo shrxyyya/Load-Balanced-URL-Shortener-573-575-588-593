33abb5972c47c44ea15b5dbd0871251ca9c183b0
Shreya <shreya5.mittal3@gmail.com>
2025-04-16 02:25:08 +0530

    Week 1 final

---
+++ url_shortener_db_3307.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE url_shortener;
+
+USE url_shortener;
+
+CREATE TABLE redirections (
+	ID INT AUTO_INCREMENT PRIMARY KEY,
+    long_url VARCHAR(1024) NOT NULL,
+    short_url VARCHAR(20) UNIQUE DEFAULT NULL
+);
+
+SELECT * FROM redirections;
+DELETE FROM redirections
